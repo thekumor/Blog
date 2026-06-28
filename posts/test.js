a409eb173106e9931715bce22034f614384b7337
Sebastian Kumór <thekumor@gmail.com>
2026-06-28 16:18:54 +0200

    Lorem ipsum
    * Made test post be lorem ipsum

--- posts/test.js
+++
@@ -1,5 +0,0 @@
-Image("https://thekumor.com/img/kumor_logo.svg");
-Text("This is some test image.");
-
-List("First element in list", "Second element", "Final element.");
-Text("Colored text :)", "#ffaa00");
