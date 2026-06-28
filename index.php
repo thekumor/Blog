90d600bef4d939d352487182294939ceea2cac22
Sebastian Kumór <thekumor@gmail.com>
2026-06-28 13:51:56 +0200

    Create index.php
    + Added an entry point

---
+++ index.php
@@ -0,0 +1,18 @@
+/* ================================================================
+*
+*	Entry point of web app.
+*
+*	#Authors: The Kumor
+*
+* ================================================================ */
+
+<html>
+<head>
+	<title>Kumor blog</title>
+	<meta charset="UTF-8" />
+</head>
+
+<body>
+	<p>Henlo :)</p>
+</body>
+</html>
