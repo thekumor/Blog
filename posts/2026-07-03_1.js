81a725b454d05bf012ac11594575d6805a3b5565
Sebastian Kumór <thekumor@gmail.com>
2026-07-03 09:38:19 +0200

    Blog post
    - Removed posts/ from gitignore
    + Added a blog post

---
+++ posts/2026-07-03_1.js
@@ -0,0 +1,15 @@
+/* --------------------------------------------------------
+*	#Section: Meta
+* -------------------------------------------------------- */
+SetTitle("GitHub redirect");
+SetAuthor("The Kumor");
+SetTime("2026-07-03 9:35");
+
+/* --------------------------------------------------------
+*	#Section: Content
+* -------------------------------------------------------- */
+Text("I made a fancy little redirect (using Apache2) to GitHub where I store projects.");
+Text("Whenever somebody writes either:");
+List("thekumor.com/g/Minesweeper or,", "thekumor.com/git/Minesweeper");
+Text("It's going to redirect them to github.com/thekumor/Minesweeper. It works for all projects, even the ones that don't exist.");
+Text("It effectively shortens time needed to get there by 3 to 5 characters.");
