fc637912e15557365554df1f63aa35a9dc87bc53
Sebastian Kumór <thekumor@gmail.com>
2026-07-04 14:19:45 +0200

    Update 2026-07-04_1.js

--- posts/2026-07-04_1.js
+++ posts/2026-07-04_1.js
@@ -10,4 +10,3 @@ SetTime("2026-07-04 14:19");
 * -------------------------------------------------------- */
 Text("Today I planned on adding two random elements for this blog. And I came up with a thing that I'll actually need: code blocks. I also added buttons, cuz why not.");
 CodeBlock("function test() {\n\tconsole.log(\"Hello World!\");\n}");
-Button("Click me!", "alert('Hello World!');");
